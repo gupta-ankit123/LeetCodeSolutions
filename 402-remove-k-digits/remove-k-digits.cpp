16165b891867cfc6e86937a255342e8e1e3ac03d
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-04-12 12:22:14 +0530

    Time: 10 ms (66.03%) | Memory: 9.8 MB (86.70%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,40 +1,36 @@
-#include <string>
-#include <stack>
-#include <algorithm>
-
 class Solution {
 public:
-    std::string removeKdigits(std::string num, int k) {
-        int n = num.size();
-        std::stack<char> st;
-        
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && k > 0 && st.top() > num[i]) {
-                st.pop();
-                k--;
-            }
-            st.push(num[i]);
+    string removeKdigits(string num, int k) {
+        int n=num.length();
+        if(n==k){
+            return "0";
         }
-        
-        while (k > 0 && !st.empty()) {
+        stack<char> st;
+        string ans;
+        for(int i=0;i<n;i++){
+            char ch=num[i];
+            while(!st.empty()&&k>0&&ch<st.top()){
+            st.pop();
+            k--;
+           }
+           if(!st.empty()||ch!='0'){
+            st.push(ch);
+           }
+           
+        }
+        while(!st.empty()&&k>0){
             st.pop();
             k--;
         }
-        
-        std::string ans;
-        while (!st.empty()) {
-            ans += st.top();
+        while(!st.empty()){
+            ans+=st.top();
             st.pop();
         }
-        
-        std::reverse(ans.begin(), ans.end());
-        
-        int i = 0;
-        while (i < ans.size() && ans[i] == '0') {
-            i++;
+        reverse(ans.begin(),ans.end());
+        if (ans.empty()) {
+            return "0";
         }
-        ans = (i == ans.size()) ? "0" : ans.substr(i);
-        
         return ans;
+        
     }
 };
