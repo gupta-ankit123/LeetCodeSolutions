712631afec05db422085b0b2d4cb1d050389a42e
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-05-27 18:52:37 +0530

    Time: 14 ms (74.60%) | Memory: 17.4 MB (70.44%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int ans=0;
+    void pathFromOneRoot(TreeNode* root, long long targetSum){
+        if(!root){
+            return;
+        }
+        if(root->val==targetSum){
+            ans++;
+        }
+        pathFromOneRoot(root->left,targetSum-root->val);
+        pathFromOneRoot(root->right,targetSum-root->val);
+    }
+
+    int pathSum(TreeNode* root, long long targetSum) {
+        if(root){
+            pathFromOneRoot(root,targetSum);
+            pathSum(root->left,targetSum);
+            pathSum(root->right,targetSum);
+
+        }
+        return ans;
+    }
+};
