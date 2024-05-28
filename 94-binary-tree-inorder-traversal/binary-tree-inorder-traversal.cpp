a81ebbf33fde656768b38a82372e9ecddbce262c
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-05-28 10:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (92.97%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,30 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        
-        stack<TreeNode*> st;
         TreeNode* curr=root;
-        while(curr!=NULL || !st.empty()){
-            while(curr!=NULL){
-                st.push(curr);
-                curr=curr->left;
+
+        while(curr){
+            //if left is NULL then visit the curr node and go right
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr=curr->right;
             }
-            curr=st.top();
-            st.pop();
-            ans.push_back(curr->val);
-            curr=curr->right;
+            //left node is not null
+            else{
+                //find inorder predecessor
+                TreeNode* pred=curr->left;
+                while(pred->right!=curr && pred->right){
+                    pred=pred->right;
+                }
+                //if right node is NULL then go left after establishing link from pred to curr
+                if(pred->right==NULL){
+                    pred->right=curr;
+                    curr=curr->left;
+                }
+
+                else{
+                    //left is already visited, go right after visiting curr node while removing the link
+                    pred->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
         }
         return ans;
     }
