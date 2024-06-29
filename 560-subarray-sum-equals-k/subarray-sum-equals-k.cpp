654a507099247056b17cb9e60f631972aff6adb7
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-06-29 14:12:47 +0530

    Time: 88 ms (44.91%) | Memory: 46.4 MB (37.92%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,11 +4,9 @@ public:
         int count=0;
         map<int,int> mpp;
         int sum=0;
+        mpp[0]=1;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum==k){
-                count++;
-            }
             int rem=sum-k;
             if(mpp.find(rem)!=mpp.end()){
                 count+=mpp[rem];
