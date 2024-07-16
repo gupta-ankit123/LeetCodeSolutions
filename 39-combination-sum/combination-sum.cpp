5f08079999ab385bc3a1081ed8a3c6bb117c0604
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-07-16 12:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (51.71%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
 
     void solve(int index,int target,vector<int>& arr,vector<vector<int>>& ans,vector<int> &ds){
-       if(index==arr.size()){
+       
         if(target==0){
             ans.push_back(ds);
+            return;
         }
-        return;
+        if(index==arr.size())
+          return;
         
-       }
+       
 
        if(arr[index]<=target){
         ds.push_back(arr[index]);
