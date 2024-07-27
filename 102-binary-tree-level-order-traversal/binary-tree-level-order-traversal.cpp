702c5d3e9ad758609fb5e5138c1cd61afaaaf413
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-07-27 18:59:37 +0530

    Time: 10 ms (11.64%) | Memory: 14.9 MB (52.59%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,43 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     void solve(TreeNode* root,vector<vector<int>>& ans){
-        if(!root){
-            return ;
-        }
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            vector<int> temp;
-            int level=q.size();
-            for(int i=0;i<level;i++){
-                TreeNode* res=q.front();
-                q.pop();
-                temp.push_back(res->val);
-                if(res->left){
-                    q.push(res->left);
-                }
-                if(res->right){
-                    q.push(res->right);
-                }
+        if(root==NULL) return;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
 
+        while(!q.empty()){
+            TreeNode* node=q.front().first;
+            int level=q.front().second;
+            q.pop();
+            if(level>=ans.size()){
+                ans.push_back(vector<int>());
             }
-            ans.push_back(temp);
-            
+        
+            ans[level].push_back(node->val);
+            if(node->left){
+                q.push({node->left,level+1});
+            }
+            if(node->right){
+                q.push({node->right,level+1});
+            }
+
         }
+
     }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
+        vector<vector<int>>ans;
+        if(root==NULL) return ans;
         solve(root,ans);
         return ans;
     }
