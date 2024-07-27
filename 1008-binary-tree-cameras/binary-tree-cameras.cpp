4441eed7f6091252fd2da27f935dea2548f97a60
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-07-27 12:45:29 +0530

    Time: 3 ms (95.30%) | Memory: 20.2 MB (50.41%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -1,38 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int cameras = 0;
-    unordered_set<TreeNode*> covered;
-
     int minCameraCover(TreeNode* root) {
-        covered.insert(nullptr);
-        dfs(root, nullptr);
-        return cameras;
+        int cameras = 0;
+        return (dfs(root, cameras) == 0) ? cameras + 1 : cameras; 
     }
 
-    void dfs(TreeNode* node, TreeNode* parent) {
-        if (node) {
-            dfs(node->left, node);
-            dfs(node->right, node);
-            if ((parent == nullptr && covered.find(node) == covered.end()) ||
-                covered.find(node->left) == covered.end() ||
-                covered.find(node->right) == covered.end()) {
-                cameras++;
-                covered.insert(node);
-                covered.insert(node->left);
-                covered.insert(node->right);
-                covered.insert(parent);
-            }
+private:
+    // 0: Needs Camera, 1: Has Camera, 2: Covered
+    int dfs(TreeNode* node, int& cameras) {
+        if (!node) {
+            return 2; 
         }
-    }
+
+        int left = dfs(node->left, cameras);
+        int right = dfs(node->right, cameras);
+
+        if (left == 0 || right == 0) { 
+            cameras++;
+            return 1; 
+        }
+
+        if (left == 1 || right == 1) { 
+            return 2; 
+        }
+
+        return 0; 
+    }
 };
