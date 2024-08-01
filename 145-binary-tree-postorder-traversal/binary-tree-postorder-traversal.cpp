2b4e1ab2b590bc2b5662d771e2653bc6296240a9
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-08-01 10:06:26 +0530

    Time: 4 ms (31.54%) | Memory: 10 MB (31.25%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,40 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
         vector<int> ans;
-        stack<TreeNode*> st1,st2;
-        st1.push(root);
-        if(root==NULL){
-            return ans;
-        }
+        TreeNode* curr=root;
 
-        while(!st1.empty()){
-            root=st1.top();
-            st1.pop();
-            st2.push(root);
+        while(!st.empty() || curr!=NULL){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                TreeNode* temp=st.top()->right;
+                if(temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right){
+                        temp=st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
 
-            if(root->left!=NULL){
-                st1.push(root->left);
-            }
-            if(root->right!=NULL){
-                st1.push(root->right);
-            }
-        }
-        while(!st2.empty()){
-            int element=st2.top()->val;
-            st2.pop();
-            ans.push_back(element);
         }
         return ans;
     }
