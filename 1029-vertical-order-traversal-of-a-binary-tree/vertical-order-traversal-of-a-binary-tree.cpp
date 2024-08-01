ee2eb7f4b89a924f7098049d02cccf88a881907a
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-08-01 20:23:47 +0530

    Time: 4 ms (56.30%) | Memory: 15.1 MB (9.73%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>> ans;
-        queue<pair<TreeNode*,pair<int,int>>>q;//Node,{roe,col}
+        vector<vector<int>>ans;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        map<int,map<int,multiset<int>>>mpp;
+
         q.push({root,{0,0}});
-        map<int,map<int,multiset<int>>>mp;
         while(!q.empty()){
-            auto front=q.front();
+            auto it=q.front();
             q.pop();
-            TreeNode*&node=front.first;
-            auto coordinate=front.second;
-            int&row=coordinate.first;
-            int&col=coordinate.second;
-            mp[col][row].insert(node->val);
+            TreeNode* node=it.first;
+            int vertical=it.second.first;
+            int level=it.second.second;
+            mpp[vertical][level].insert(node->val);
+
             if(node->left){
-                q.push({node->left,{row+1,col-1}});
+                q.push({node->left,{vertical-1,level+1}});
             }
             if(node->right){
-                q.push({node->right,{row+1,col+1}});
+                q.push({node->right,{vertical+1,level+1}});
             }
-        }
-        //store final vertical order into ans vector
-        for(auto it:mp){
-            auto&colMap=it.second;
-            vector<int> vLine;
-            for(auto colMapIt:colMap){
-                auto &mset=colMapIt.second;
-                vLine.insert(vLine.end(),mset.begin(),mset.end());
 
+        }
+        for(auto it:mpp){
+            vector<int>temp;
+            for(auto p:it.second){
+                temp.insert(temp.end(),p.second.begin(),p.second.end());
             }
-            ans.push_back(vLine);
+            ans.push_back(temp);
         }
         return ans;
-
     }
 };
