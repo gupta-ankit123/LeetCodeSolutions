f7e83df0ea7fc7ba357c2dc4540726c04c20fbac
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-08-02 16:38:39 +0530

    Time: 188 ms (5.79%) | Memory: 26.8 MB (15.46%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,32 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
 
-    int solve(TreeNode* root,int &ans){
-        if(!root){
-            return 0;
-        }
-        int left=max(0,solve(root->left,ans));//negative wale ko neglect karna hai
-        int right=max(0,solve(root->right,ans));
-       
-        ans=max(ans,(root->val+left+right));
-        return root->val+max(left,right);
-
+    int path(TreeNode* root){
+        if(root==NULL) return 0;
+        int left=max(0,path(root->left));
+        int right=max(0,path(root->right));
+        int pathSum=root->val+max(left,right);
+        return pathSum;
     }
     int maxPathSum(TreeNode* root) {
+        if(root==NULL) return INT_MIN;
+        if(root->right==NULL && root->left==NULL) return root->val;
+        int leftSum=max(0,path(root->left));
+        int rightSum=max(0,path(root->right));
+        int sum=root->val+leftSum+rightSum;
+        sum=max(sum,root->val);
 
-        int ans=INT_MIN;
-        solve(root,ans);
-        return ans;
+        int left=maxPathSum(root->left);
+        int right=maxPathSum(root->right);
+        return max({sum,left,right});
     }
 };
