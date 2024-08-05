11428c67abc8f56f416f37cf17770e1084575cb6
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-08-05 20:49:01 +0530

    Time: 11 ms (36.32%) | Memory: 13 MB (28.19%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -16,8 +16,24 @@ public:
     int minDistance(string s1, string s2) {
         int n=s1.length();
         int m=s2.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,s1,s2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                 if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1];
+
+                else {
+                    int insertion=dp[i][j-1];
+                    int deletion=dp[i-1][j];
+                    int replace=dp[i-1][j-1];
+
+                    dp[i][j]=1+min({insertion,deletion,replace});
+                }
+            }
+        }
+        return dp[n][m];
 
     }
 };
