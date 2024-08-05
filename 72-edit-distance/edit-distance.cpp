7eb7000820cb19a422609cdf7d4e1e0d4c72a6e1
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-08-05 20:40:50 +0530

    Time: 6 ms (71.08%) | Memory: 13.7 MB (19.23%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int solve(int i,int j,string &s1, string &s2,vector<vector<int>>&dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        if(i==0) return j;
+        if(j==0) return i;
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s1[i]==s2[j]) return dp[i][j]=solve(i-1,j-1,s1,s2,dp);
+        if(s1[i-1]==s2[j-1]) return dp[i][j]=solve(i-1,j-1,s1,s2,dp);
 
         int insertion=solve(i,j-1,s1,s2,dp);
         int deletion=solve(i-1,j,s1,s2,dp);
@@ -17,7 +17,7 @@ public:
         int n=s1.length();
         int m=s2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(n-1,m-1,s1,s2,dp);
+        return solve(n,m,s1,s2,dp);
 
     }
 };
