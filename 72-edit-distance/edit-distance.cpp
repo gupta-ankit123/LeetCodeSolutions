81d71857339686ca2569b387bfc7fe5e7103676f
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-08-05 20:37:58 +0530

    Time: 9 ms (49.51%) | Memory: 13.4 MB (20.10%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -5,7 +5,7 @@ public:
         if(j<0) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s1[i]==s2[j]) return solve(i-1,j-1,s1,s2,dp);
+        if(s1[i]==s2[j]) return dp[i][j]=solve(i-1,j-1,s1,s2,dp);
 
         int insertion=solve(i,j-1,s1,s2,dp);
         int deletion=solve(i-1,j,s1,s2,dp);
