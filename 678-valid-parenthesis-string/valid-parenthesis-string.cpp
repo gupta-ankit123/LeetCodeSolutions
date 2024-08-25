590a0c488e2a1fd933b2cd57c09dd7253dd05bc6
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-08-25 14:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (17.44%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -3,7 +3,6 @@ public:
     bool checkValidString(string s) {
         stack<char> st;
         stack<char> star;
-
         for(int i=0;i<s.length();i++){
             if(s[i]=='('){
                 st.push(i);
@@ -11,8 +10,8 @@ public:
             else if(s[i]=='*'){
                 star.push(i);
             }
-            else if(s[i]==')'){
-                if(st.empty()&&star.empty()){
+            else if(s[i]==')') {
+                if(st.empty() && star.empty()){
                     return false;
                 }
                 else if(!st.empty()){
@@ -25,15 +24,12 @@ public:
         }
 
         while(!st.empty()){
-            if(star.empty()){
-                return false;
-            }
-            if(st.top()>star.top()){
-                return false;
-            }
+            if(star.empty()) return false;
+
+            if(st.top()>star.top()) return false;
+
             st.pop();
             star.pop();
-
         }
         return true;
     }
