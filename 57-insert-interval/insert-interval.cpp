12d9200205114428d723684431913ff29178f55c
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-09-06 16:53:08 +0530

    Time: 7 ms (91.40%) | Memory: 20.8 MB (68.51%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int n=arr.size();
+        int i=0;
+        while(i<n && arr[i][1]<newInterval[0]){
+            ans.push_back(arr[i]);
+            i++;
+        }
+
+        while(i<n && arr[i][0]<=newInterval[1]){
+            newInterval[0]=min(arr[i][0],newInterval[0]);
+            newInterval[1]=max(arr[i][1],newInterval[1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+        while(i<n){
+            ans.push_back(arr[i]);
+            i++;
+        }
+        return ans;
+    }
+};
