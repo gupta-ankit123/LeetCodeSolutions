bae7342b3dec8a42885f574233100cd99211a6bd
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-09-15 20:27:25 +0530

    Time: 64 ms (8.97%) | Memory: 34.1 MB (27.11%) - LeetSync

---
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countConsistentStrings(string s, vector<string>& words) {
+        map<char,int> mpp;
+        for(int i=0;i<s.length();i++){
+            mpp[s[i]]++;
+        }
+        int cnt=0;
+        for(auto it:words){
+            bool flag=true;
+            for(int i=0;i<it.length();i++){
+                if(mpp.find(it[i])==mpp.end()){
+                    flag=false;
+                }
+            }
+            if(flag){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
