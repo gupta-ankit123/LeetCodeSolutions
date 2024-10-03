551ab020da9c2a056fc10293346e6b793e9950d6
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-10-03 11:52:00 +0530

    Time: 4 ms (42.54%) | Memory: 13.9 MB (52.65%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int e=n-1;
+        int s=0;
+       
+
+        while(s<=e){
+             int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && target<=nums[mid]){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[e]){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+};
