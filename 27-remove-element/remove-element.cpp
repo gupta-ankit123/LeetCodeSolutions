96b5bb85dc1db28714599f2207a40aed605d3f63
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-10-03 17:23:59 +0530

    Time: 5 ms (27.98%) | Memory: 10.7 MB (37.18%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int x=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=val){
+                swap(nums[i],nums[x]);
+                x++;
+            }
+        }
+        return x;
+
+    }
+};
+
+
+
